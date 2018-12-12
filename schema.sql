de1dba5ae53c9a0b3307da74ad850ff721c602f9
stevelucascs <stevelucascs@gmail.com>
2018-12-12 17:35:05 -0600

    updated schema to include supervisor

--- schema.sql
+++ schema.sql
@@ -3,12 +3,21 @@ CREATE DATABASE myzon_db;
 USE myzon_db;
 
 CREATE TABLE products (
-  item_id INT NOT NULL AUTO_INCREMENT,
+  item_id INT(11) NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
-  department_name VARCHAR(100) NULL,
-  price DECIMAL(10,2) NOT NULL DEFAULT 0,
-  stock INT NOT NULL DEFAULT 0,
+  department_name VARCHAR(40) NULL,
+  unit_cost DECIMAL(10,2) NOT NULL DEFAULT 1,
+  stock_quantity INT(11) NOT NULL DEFAULT 0,
+  quantity_sold INT(11) NOT NULL DEFAULT 0,
+  sale_price DECIMAL(10,2) NOT NULL DEFAULT 2,
   PRIMARY KEY (item_id)
 );
 
-SELECT * FROM products;
+CREATE TABLE supervisor (
+  department_id INT(11) NOT NULL AUTO_INCREMENT,
+  department_name varChar(40) NULL,
+  cost INT(11) NOT NULL DEFAULT 0,
+  revenue INT(11) NOT NULL DEFAULT 0,
+  profit INT(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (department_id)
+);
